b44b44008ec3a7ec84c00e03019d3bcb55b220d8
Felix Galvin <felixgalvin1709@gmail.com>
2024-11-27 11:37:41 +0700

    toString

--- src/harapanbangsachicken/model/classes/Customer.java
+++ src/harapanbangsachicken/model/classes/Customer.java
@@ -13,8 +13,6 @@ public class Customer extends User {
     private ArrayList<Promo> listPromo;
     private ArrayList<Reward> reward;
 
- 
-
     public Customer(int user_id, String namaDepan, String namaBelakang, String password, String email, String noTelp,
             String alamat, String gender, double saldo, ArrayList<History> history, ArrayList<Promo> listPromo,
             int point, Level level, ArrayList<Reward> reward) {
@@ -32,49 +30,85 @@ public class Customer extends User {
     public String getAlamat() {
         return alamat;
     }
+
     public void setAlamat(String alamat) {
         this.alamat = alamat;
     }
+
     public String getGender() {
         return gender;
     }
+
     public void setGender(String gender) {
         this.gender = gender;
     }
+
     public double getSaldo() {
         return saldo;
     }
+
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
+
     public ArrayList<History> getHistory() {
         return history;
     }
+
     public void setHistory(ArrayList<History> history) {
         this.history = history;
     }
+
     public ArrayList<Promo> getListPromo() {
         return listPromo;
     }
+
     public void setListPromo(ArrayList<Promo> listPromo) {
         this.listPromo = listPromo;
     }
+
     public int getPoint() {
         return point;
     }
+
     public void setPoint(int point) {
         this.point = point;
     }
+
     public Level getLevel() {
         return level;
     }
+
     public void setLevel(Level level) {
         this.level = level;
     }
+
     public ArrayList<Reward> getReward() {
         return reward;
     }
+
     public void setReward(ArrayList<Reward> reward) {
         this.reward = reward;
+    }
+
+
+    @Override
+    public String toString() {
+        String historyMsg = "";
+        String promoMsg = "";
+        String rewardMsg = "";
+
+        for (History msg1 : history) {
+            historyMsg += msg1.toString();
+        }
+
+        for (Promo msg2 : listPromo) {
+            promoMsg += msg2.toString();
+        }
+
+        for (Reward msg3 : reward) {
+            rewardMsg += msg3.toString();
+        }
+        return super.toString() + "\nAlamat: " + alamat + "\nGender: " + gender + "\nSaldo: " + saldo + "\nLevel: " + level + "\nPoint: " + point + "\nHistory: " + historyMsg + "\nPromo: " + promoMsg + "\nReward: " + rewardMsg;
     }
 }
--- src/harapanbangsachicken/model/classes/User.java
+++ src/harapanbangsachicken/model/classes/User.java
@@ -8,8 +8,6 @@ public abstract class User {
     private String email;
     private String noTelp;
 
-
-
     public User(int user_id, String namaDepan, String namaBelakang, String password, String email, String noTelp) {
         this.user_id = user_id;
         this.namaDepan = namaDepan;
@@ -22,36 +20,47 @@ public abstract class User {
     public int getUser_id() {
         return user_id;
     }
+
     public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
+
     public String getNamaDepan() {
         return namaDepan;
     }
+
     public void setNamaDepan(String namaDepan) {
         this.namaDepan = namaDepan;
     }
+
     public String getNamaBelakang() {
         return namaBelakang;
     }
+
     public void setNamaBelakang(String namaBelakang) {
         this.namaBelakang = namaBelakang;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getNoTelp() {
         return noTelp;
     }
+    
     public void setNoTelp(String noTelp) {
         this.noTelp = noTelp;
     }
@@ -60,4 +69,7 @@ public abstract class User {
         return this.email.equals(inputEmail) && this.password.equals(inputPassword);
     }
 
+    public String toString() {
+        return "User ID: " + user_id + "\nNama Depan: " + namaDepan + "\nNama Belakang: " + namaBelakang + "\nPassword: " + password + "\nEmail: " + email + "\nNomor Telepon: " + noTelp;
+    }
 }
